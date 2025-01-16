08cbef8bff60451966bc208e4ff38dcf3f9e7242
Martin Miloslav Suquillo Tigasi <116769944+31MartinS@users.noreply.github.com>
2025-01-15 22:28:41 -0500

    v1

---
+++ GestionProductos/TestProjectGestionProductos/ProductControllerTest.cs
@@ -0,0 +1,21 @@
+﻿using GestionProductos;
+using Xunit;
+
+namespace TestProjectGestionProductos
+{
+    public class ProductoControllerTest
+    {
+        [Fact]
+
+        public void Test1()
+        {
+            //configurar
+            var producto = new Producto { Nombre = "Prueba", Precio = 10 };
+
+            //validar
+            Assert.True(producto.Precio >= 0, "El precio no pyuede ser negativo");
+        }
+
+    }
+
+}
